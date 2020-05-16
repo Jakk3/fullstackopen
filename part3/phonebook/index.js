87e40ebce5bc36aed0fa66ecfa59b0eb07e94dff
Jakk3 <32640696+Jakk3@users.noreply.github.com>
2020-05-17 02:59:50 +0300

    part 3a

---
+++ part3/phonebook/index.js
@@ -0,0 +1,88 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+
+app.use(express.json())
+
+morgan.token('body', function (req, res) {
+  return JSON.stringify(req.body)
+})
+
+app.use(morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    tokens.body(req, res),
+  ].join(' ')
+}))
+
+const port = process.env.PORT || 80
+
+let persons = [
+  {
+    "name": "Arto Hellas",
+    "number": "441-41415-151",
+    "id": 1
+  },
+  {
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523",
+    "id": 2
+  },
+  {
+    "name": "Alexander Hamilton",
+    "number": "312-414-6151",
+    "id": 3
+  },
+  {
+    "name": "Thomas Jefferson",
+    "number": "414-4141-6262",
+    "id": 4
+  }
+]
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(p => p.id === id)
+  if (person)
+    res.json(person)
+  else
+    res.status(404).end()
+})
+
+app.post('/api/persons', (req, res) => {
+  const id = Math.floor(Math.random() * 999999)
+  const name = req.body.name
+  const number = req.body.number
+
+  if (persons.map(p => p.name === name)) {
+    res.status(400).json({ error: 'name must be unique' })
+  } else if (name && number) {
+    persons.push({ name, number, id })
+    res.status(200).end()
+  } else
+    res.status(400).json({ error: 'name or number missing' })
+
+
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
+})
+
+app.get('/info', (req, res) => {
+  res.send("<p> Phonebook has info for " + persons.length + " people.</p> " + "<p>" + Date() + "</p>"
+  )
+})
+
+app.listen(port)
---
+++ part3/phonebook/requests/README.md
@@ -0,0 +1,4 @@
+# REST Client
+Rest Client is Visual Studio Code extension that can be used to make API calls for testing purposes right on the editor.
+
+[Download REST Client](https://marketplace.visualstudio.com/items?itemName=humao.rest-client)
