88f9c4746c88d4ca24780c66218998470feef264
Jakk3 <32640696+Jakk3@users.noreply.github.com>
2020-06-19 16:32:06 +0300

    part 7b 7.8

--- part7/ultimate-hooks/src/App.js
+++ part7/ultimate-hooks/src/App.js
@@ -1,4 +1,4 @@
-  
+
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
@@ -20,10 +20,17 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  useEffect(() => {
+    axios
+      .get(baseUrl)
+      .then((res) => {
+        setResources(res.data)
+      })
+  }, [])
 
-  const create = (resource) => {
-    // ...
+  const create = async (resource) => {
+    const data = await axios.post(baseUrl, resource)
+    setResources([...resources, data])
   }
 
   const service = {
@@ -47,10 +54,10 @@ const App = () => {
     event.preventDefault()
     noteService.create({ content: content.value })
   }
- 
+
   const handlePersonSubmit = (event) => {
     event.preventDefault()
-    personService.create({ name: name.value, number: number.value})
+    personService.create({ name: name.value, number: number.value })
   }
 
   return (
@@ -64,7 +71,7 @@ const App = () => {
 
       <h2>persons</h2>
       <form onSubmit={handlePersonSubmit}>
-        name <input {...name} /> <br/>
+        name <input {...name} /> <br />
         number <input {...number} />
         <button>create</button>
       </form>
